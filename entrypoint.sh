6454bf75981972648c87d0b801bdb1c9c7b137d6
Branko Wilhelm <branko.wilhelm@gmail.com>
2022-04-22 19:35:36 +0200

    fix cron not running

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "startup $0"
 
 if [ ! -z "${SCHEDULE}" ] &&  [ ! -z "${CRON_CMD}" ]; then
   echo "configure cron: ${SCHEDULE} ${CRON_CMD}"
-  echo "${SCHEDULE} ${CRON_CMD}" > /etc/cron.d/s3cmd
+  echo "${SCHEDULE} ${CRON_CMD}" > /var/spool/cron/crontabs/root
 fi
 
 echo "run: $@"
